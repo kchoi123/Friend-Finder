f8e8b0bbaf8b8b4d1b88ce3d349be7f2e763f8d5
kchoi123 <k3vinch0i@gmail.com>
2019-03-26 16:32:01 -0700

    update folders

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ var path = require("path");
 
 // Setup express app
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Strat the server to begin listening
+app.listen(PORT, function () {
+    console.log("Starting!");
+    console.log("App listening on PORT " + PORT);
+});
+
+// Starting the home page "/" or index.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'app', 'public', "home.html"));
+});
+
+// Enter survey
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, 'app', 'public', "survey.html"));
+  });
