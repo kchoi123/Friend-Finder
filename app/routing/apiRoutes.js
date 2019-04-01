5ce9a996cbb539329282f58182b1453e6f3c3420
Kevin <k3vinch0i@gmail.com>
2019-03-31 17:24:57 -0700

    made changes to api and html routes. running node and webpages are able to navigate through

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,41 @@
-// Displays all subjects
-app.get("/api/subjects", function(req, res) {
+// Load Data
+var subjects = require("../data/friends");
+
+module.exports = function (app) {
+  // Displays all subjects
+  app.get("/api/subjects", function (req, res) {
     return res.json(subjects);
   });
-  
+
   // Displays a single subject, or returns false
-  app.get("/api/subjects/:subject", function(req, res) {
+  app.get("/api/subjects/:subject", function (req, res) {
     var chosen = req.params.subject;
-  
+
     console.log(chosen);
-  
+
     for (var i = 0; i < subjects.length; i++) {
       if (chosen === subjects[i].routeName) {
         return res.json(subjects[i]);
       }
     }
-  
+
     return res.json(false);
   });
-  
+
   // Create New Subjects - takes in JSON input
-  app.post("/api/subjects", function(req, res) {
+  app.post("/api/subjects", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newsubject = req.body;
-  
+
     // Using a RegEx Pattern to remove spaces from newsubject
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newsubject.routeName = newsubject.name.replace(/\s+/g, "").toLowerCase();
-  
+
     console.log(newsubject);
-  
+
     subjects.push(newsubject);
-  
+
     res.json(newsubject);
   });
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,12 +5,12 @@ var path = require("path");
 module.exports = function (app) {
 
     // Home Page
-    app.get("/tables", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     // Enter survey
-    app.get("/reserve", function (req, res) {
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
