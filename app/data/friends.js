b184372ad6d6feeccbcba6ed8553fb9183e3867b
kchoi123 <k3vinch0i@gmail.com>
2019-04-01 00:49:40 -0700

    edits to friends matching equation

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,38 @@
 var subjects = [{
-        name: "Steph",
+        name: "Roockie",
         photo: "https://images.tmz.com/2017/03/27/0327-murphy-doodles-3.jpg",
         scores: [
-            "5",
             "1",
-            "4",
+            "2",
+            "3",
             "4",
             "5",
             "1",
             "2",
-            "5",
+            "3",
             "4",
-            "1"
+            "5"
         ]
     },
     {
         name: "POLO",
-        photo: "https://www.instagram.com/p/Bswp3WrgtqW/?utm_source=ig_web_options_share_sheet",
+        photo: "https://i.pinimg.com/originals/00/32/25/003225b0b077cb7d0b1ce58b56f2ac51.jpg",
         scores: [
             "5",
-            "1",
             "4",
-            "4",
-            "5",
-            "1",
+            "3",
             "2",
+            "1",
             "5",
             "4",
+            "3",
+            "2",
             "1"
         ]
     },
     {
         name: "Mocha",
-        photo: "https://www.instagram.com/p/BcxndqInsAZ/?utm_source=ig_web_options_share_sheet",
+        photo: "https://dgicdplf3pvka.cloudfront.net/919290/shiba-inu-puppy-picture-6523488e-334e-4d70-8ed5-31315fe7a876.jpg",
         scores: [
             "5",
             "1",
@@ -48,18 +48,18 @@ var subjects = [{
     },
     {
         name: "Nacho",
-        photo: "https://www.instagram.com/p/BouQInjHZbN/?utm_source=ig_web_options_share_sheet",
+        photo: "https://i.pinimg.com/originals/74/61/9a/74619a768fee5145eb6f2aa8be762e2d.jpg",
         scores: [
             "5",
-            "1",
-            "4",
-            "4",
-            "5",
-            "1",
             "2",
-            "5",
+            "3",
             "4",
-            "1"
+            "5",
+            "2",
+            "2",
+            "4",
+            "4",
+            "2"
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,36 @@ module.exports = function (app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    subjects.push(req.body);
-    console.log(req);
-    console.log(res);
     
+    // console.log("dog has been added");
+
+    // console.log(req.body.scores);
+
+    var name = "";
+    var photo = "";
+
+    // 5*10 = 50, is the largest number but going to use double that, 100
+    var total = 100;
+
+    for (var i = 0; i < subjects.length; i++) {
+      var compareTotal = 0;
+      
+			for (var j = 0; j < req.body.scores.length; j++) {
+				compareTotal += Math.abs(subjects[i].scores[j] - req.body.scores[j]);
+			}
+
+			if (compareTotal < total) {
+
+				total = compareTotal;
+				name = subjects[i].name;
+				photo = subjects[i].photo;
+			}
+    }
+    
+    res.json({name: name, photo: photo});
+
+    // push new data after you select choice
+    subjects.push(req.body);
+
   });
 };
